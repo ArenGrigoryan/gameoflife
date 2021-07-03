9088b0e9c7087ed8c6e0bc0f643ae15e3b7e0291
ArenGrigoryan <aren.grigoryan2.s@tumo.org>
2021-07-03 17:00:55 +0400

    blahblahblah

---
+++ public/components/Grass.js
@@ -0,0 +1,18 @@
+class Grass extends Component {
+	constructor (x, y) {
+		super(x, y)
+		this.time = 0
+		this.directions = genDirs(x, y)
+	}
+
+	action () {
+		this.time++
+		const cells = chooseCells(0, this.directions)
+		const randomCell = cells[Math.floor(Math.random() * cells.length)]
+		if (this.time >= 7 && randomCell) {
+			field[randomCell[1]][randomCell[0]] = 1
+			grasses.push(new Grass(randomCell[0], randomCell[1]))
+			this.time = 0
+		}
+	}
+}
